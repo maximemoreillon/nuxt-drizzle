52a25b40e3bb4b0e096b66e123a67850c9d9ef4c
Maxime Moreillon <m.moreillon@gmail.com>
2024-06-05 16:10:27 +0900

    param handling

--- server/api/movies/[id]/index.delete.ts
+++ server/api/movies/[id]/index.delete.ts
@@ -2,12 +2,11 @@ import { db } from "@/server/db"
 import { eq } from "drizzle-orm"
 import { Movies } from "~/db/schema"
 
-export default defineEventHandler(async ({ context }) => {
-  if (!context.params) throw "Params not defined"
-  const { id } = context.params
+export default defineEventHandler(async (event) => {
+  const id = getRouterParam(event, "id")
   const [movie] = await db
-    .select()
-    .from(Movies)
+    .delete(Movies)
     .where(eq(Movies.id, Number(id)))
+    .returning()
   return movie
 })
